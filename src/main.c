3f18cca30a80b33be455d9e507f8b8c4e2dce58f
inspiredgoat <tomd@airmail.cc>
2020-10-20 23:40:48 +1100

    tried adding some matrix stuff, didn't exactly go to plan

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
 #include "../include/types.h"
 
+
+#define PI 3.14159
+
 #define debug_print(x) _debug_print(x, __LINE__, __FILE__);
 #define this_ran() _debug_print("This ran", __LINE__, __FILE__);
+
+
+//------------------------------------------------------------------------
 
 void _debug_print(const char* message, uint line, const char* filename) {
 	printf("%s, LINE: %u, FILE: %s\n", message, line, filename);
@@ -15,6 +22,9 @@ void _debug_print(const char* message, uint line, const char* filename) {
 void framebuffer_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
+
+//------------------------------------------------------------------------
+
 
 Buffer load_file(const char* filename) {
 	Buffer buf;
@@ -87,6 +97,10 @@ uint load_shader(const char* vertex_shader_file, const char* frag_shader_file) {
 }
 
 
+
+//------------------------------------------------------------------------
+
+
 int main() {
 	// Window setup
 
@@ -118,15 +132,41 @@ int main() {
 	uint vbo;
 
 	float verts[] = {
-		-0.5f, -0.5f, 0.f,
-		 0.5f, -0.5f, 0.f,
-		-0.5f,  0.5f, 0.f,
-		 0.5f,  0.5f, 0.f,
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
 	};
 	
 	uint indices[] = {
+		// front
+		1, 2, 3,
 		0, 1, 3,
-		1, 2, 3
+
+		// left
+		1, 5, 2,
+		5, 6, 2,
+
+		// right
+		4, 0, 7,
+		0, 3, 7,
+
+		// back
+		5, 4, 6,
+		4, 7, 6,
+		
+		// top
+		4, 5, 0,
+		5, 1, 0,
+
+		// bottom
+		3, 2, 7,
+		2, 6, 7
 	};
 
 	glGenVertexArrays(1, &vao);
@@ -139,23 +179,39 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	float transform[16] = {
+		1.f, 0.f, 0.f, 0.f,
+		0.f, 1.f, 0.f, 0.f,
+		0.f, 0.f, 1.f, 0.f,
+		0.f, 0.f, 0.f, 1.f
+	};
+
+//	float angle = PI * 0.25f;
+//	transform[5] =   cos(angle);
+//	transform[6] =  -sin(angle);
+//	transform[9] =   sin(angle);
+//	transform[10] =  cos(angle);
+
+	glUniformMatrix4fv(glGetUniformLocation(shader_program, "transform"), 1, GL_FALSE, transform);
+
 	glUseProgram(shader_program);
 	glBindVertexArray(vao);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while(!glfwWindowShouldClose(window)) {
-		glClearColor(0, 0, 0, 1);
+		glfwSwapBuffers(window);
+		glClearColor(.25, .5, .25, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
